ae3c57b6bf7e73ee8156c941b4bb0c4434b0ebf4
Rakshitih <rakshithkumab01718@gmail.com>
2026-02-17 17:32:11 +0530

    feat: implement user authentication module including user registration, login, JWT token generation, and basic authentication middleware.

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -32,10 +32,68 @@ exports.registerUser = async (req, res) => {
             },
             process.env.JWT_SECRET,
             {
-                expiresIn: "1h"
+                expiresIn: "7d"
             }
-        )
+        );
+
+        res.status(201).json({
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+            },
+        });
     } catch (error) {
-        
+        res.status(500).json({
+            message:"Server error"
+        })
+    }
+};
+
+// LOGIN
+exports.loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid Credentials"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid Credentials"
+            
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: user._id
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d"
+            }
+        );
+
+        res.status(200).json({
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error"
+        })
     }
 }
---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+
+
+modlue.exports = (req, res, next) => {
+    let token = req.header("Authorization");
+
+    if(!token) {
+        return res.status(401).json({
+            message: "Unauthorized"
+        
+        })
+    }
+    try {
+        token = token.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.id;
+        next(); 
+    } catch (error) {
+        res.status(401).json({
+            message: "Token is not valid"
+        })
+    }
+}
